47bab55b68d7efca70c4f93af4a5663d51aa3764
Jonata Santiago <jonata_santiago@hotmail.com>
2023-02-26 03:01:38 -0300

    Inclusão do atributo idade.

--- ProjetoCurso/src/Pessoa/Cliente.java
+++ ProjetoCurso/src/Pessoa/Cliente.java
@@ -5,6 +5,16 @@ public class Cliente {
 	private string nome;
 	
 	private string sobrenome;
+	
+	private int idade;
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 	public string getNome() {
 		return nome;
