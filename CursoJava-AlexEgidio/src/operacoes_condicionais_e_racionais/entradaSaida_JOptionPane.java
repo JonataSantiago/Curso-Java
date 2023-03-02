59e935a5317f24a734b9604025c88370fe092c16
Jonata Santiago <jonata_santiago@hotmail.com>
2023-03-01 23:35:49 -0300

    Fechamento do Módulo: Operações lógicas, condicionais e relacionais.

---
+++ CursoJava-AlexEgidio/src/operacoes_condicionais_e_racionais/entradaSaida_JOptionPane.java
@@ -0,0 +1,53 @@
+package operacoes_condicionais_e_racionais;
+
+import javax.swing.JOptionPane;
+
+public class entradaSaida_JOptionPane {
+
+	public static void main(String[] args) {
+	
+	/* Módulo: Resto da Divisão % */	
+	/*
+	 
+	double carro = 9;
+	double pessoa = 2;
+	
+	double resto = carro % pessoa;
+	
+	System.out.println("Sobraram exatamente: " + resto + " carros" );
+	
+	*/
+	
+	/*---------------------------------------------------------------------*/	
+	
+	/* Entrada / Saída e Confirmão de dados através da classe JOptionPane (Caixas de Diálogos */
+		
+	String carros = JOptionPane.showInputDialog("Informe a quantidade de carros?");
+	String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas?");
+	
+	double carroNumero = Double.parseDouble(carros);
+	double pessoaNumero = Double.parseDouble(pessoas);
+	
+	int divisao = (int) (carroNumero / pessoaNumero);
+	
+	double resto = carroNumero % pessoaNumero;
+	
+		/*Imprime caixa de confirmação: Sim=0 /Não=1 /Cancelar=2 */
+	int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão? ");
+	
+	if (resposta == 0) {
+		/*Imprime caixa de menssagem na tela*/
+		JOptionPane.showMessageDialog(null, "Divisão por pessoa deu : " +  divisao);
+	}
+	
+	resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resto da divisão? ");
+	
+	if (resposta == 0) {
+		JOptionPane.showMessageDialog(null, "O resto é : " +  resto);
+	}
+	
+	
+	
+	}
+
+}
